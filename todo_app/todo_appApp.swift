0f7e68feaca5dcf6f3751566ce99d22a71483996
kobayashiharuto <datesaver1234@gmail.com>
2021-03-28 21:04:43 +0900

    Initial Commit

---
+++ todo_app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo_app
+//
+//  Created by 小林春斗 on 2021/03/28.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
